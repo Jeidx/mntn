8c18cad6dd99807243f094e0cb39af207f0c7ad9
“Vasya <jedix1488@gmail.com>
2021-05-17 09:45:35 +0300

    add dist first version

---
+++ docs/dist/js/app.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.mySwiper', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
